a2303020b765997d36920fd9106acb6187cfb186
Titus Tienaah <titus.intdxdt@gmail.com>
2017-12-30 13:43:29 -0400

    updated contiguous ordering

--- merge.go
+++ merge.go
@@ -1,80 +1,81 @@
 package merge
 
 import (
-    "simplex/rng"
-    "simplex/node"
-    "simplex/lnr"
-    "simplex/knn"
+	"simplex/rng"
+	"simplex/node"
+	"simplex/lnr"
+	"simplex/knn"
 	"simplex/common"
-    "github.com/intdxdt/sset"
-    "github.com/intdxdt/rtree"
-    "github.com/intdxdt/geom"
+	"github.com/intdxdt/sset"
+	"github.com/intdxdt/rtree"
+	"github.com/intdxdt/geom"
 	"sort"
 )
 
-
-
 //Merge two ranges
 func Range(ra, rb *rng.Range) *rng.Range {
-    var ranges = common.SortInts(append(ra.AsSlice(), rb.AsSlice()...))
-    // i...[ra]...k...[rb]...j
-    return rng.NewRange(ranges[0], ranges[len(ranges)-1])
+	var ranges = common.SortInts(append(ra.AsSlice(), rb.AsSlice()...))
+	// i...[ra]...k...[rb]...j
+	return rng.NewRange(ranges[0], ranges[len(ranges)-1])
 }
 
 //Merge contiguous fragments based combined score
 func ContiguousFragmentsAtThreshold(
-    scoreFn lnr.ScoreFn, ha, hb *node.Node,
-    scoreRelation func(float64) bool, gfn geom.GeometryFn,
+	scoreFn lnr.ScoreFn, ha, hb *node.Node,
+	scoreRelation func(float64) bool, gfn geom.GeometryFn,
 ) *node.Node {
-    if !ha.Range.Contiguous(hb.Range) {
-        panic("node are not contiguous")
-    }
-    var coordinates = ContiguousCoordinates(ha, hb)
-    _, val := scoreFn(coordinates)
-    if scoreRelation(val) {
-        return contiguousFragments(coordinates, ha, hb, gfn)
-    }
-    return nil
+	if !ha.Range.Contiguous(hb.Range) {
+		panic("node are not contiguous")
+	}
+	var coordinates = ContiguousCoordinates(ha, hb)
+	_, val := scoreFn(coordinates)
+	if scoreRelation(val) {
+		return contiguousFragments(coordinates, ha, hb, gfn)
+	}
+	return nil
 }
 
 func ContiguousCoordinates(ha, hb *node.Node) []*geom.Point {
-    if !ha.Range.Contiguous(hb.Range) {
-        panic("node are not contiguous")
-    }
-    var nodes = []*node.Node{ha, hb}
-    sort.Sort(node.Nodes(nodes))
-    var na, nb = nodes [0], nodes[1]
-    var coordinates = na.Coordinates()
-    var n = len(coordinates) - 1
-    coordinates = append(coordinates[:n:n], nb.Coordinates()...)
-    return coordinates
+	if !ha.Range.Contiguous(hb.Range) {
+		panic("node are not contiguous")
+	}
+	//var nodes = []*node.Node{ha, hb}
+	//sort.Sort(node.Nodes(nodes))
+	if hb.Range.I < ha.Range.J && hb.Range.J == ha.Range.I {
+		ha, hb = hb, ha
+	}
+
+	var coordinates = ha.Coordinates()
+	var n = len(coordinates) - 1
+	coordinates = append(coordinates[:n:n], hb.Coordinates()...)
+	return coordinates
 }
 
 //Merge contiguous hulls
 func contiguousFragments(
-    coordinates []*geom.Point,
-    ha, hb *node.Node,
-    gfn geom.GeometryFn,
+	coordinates []*geom.Point,
+	ha, hb *node.Node,
+	gfn geom.GeometryFn,
 ) *node.Node {
-    var r = Range(ha.Range, hb.Range)
-    // i...[ha]...k...[hb]...j
-    return node.New(coordinates, r, gfn)
+	var r = Range(ha.Range, hb.Range)
+	// i...[ha]...k...[hb]...j
+	return node.New(coordinates, r, gfn)
 }
 
 //Merge contiguous hulls by fragment size
 func ContiguousFragmentsBySize(
-    hulls []*node.Node,
-    hulldb *rtree.RTree,
-    vertexSet *sset.SSet,
-    unmerged map[[2]int]*node.Node,
-    fragmentSize int,
-    isScoreValid func(float64) bool,
-    scoreFn lnr.ScoreFn,
-    gfn geom.GeometryFn,
-    EpsilonDist float64,
+	hulls []*node.Node,
+	hulldb *rtree.RTree,
+	vertexSet *sset.SSet,
+	unmerged map[[2]int]*node.Node,
+	fragmentSize int,
+	isScoreValid func(float64) bool,
+	scoreFn lnr.ScoreFn,
+	gfn geom.GeometryFn,
+	EpsilonDist float64,
 ) ([]*node.Node, []*node.Node) {
 
-    //@formatter:off
+	//@formatter:off
 	var keep = make([]*node.Node, 0)
 	var rm = make([]*node.Node, 0)
 
@@ -116,7 +117,7 @@ func ContiguousFragmentsBySize(
 			//its mergeable : mergeable score <= threshold
 			var mergeable = (
 				(hr.J == sr.I && !vertexSet.Contains(sr.I)) ||
-				(hr.I == sr.J && !vertexSet.Contains(sr.J)))
+					(hr.I == sr.J && !vertexSet.Contains(sr.J)))
 
 			if mergeable {
 				var _, val = scoreFn(ContiguousCoordinates(s, h))
